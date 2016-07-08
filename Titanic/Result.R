7fa9c4009d8b2577f2e0245329b7aa5e4e6a2633
nathanLee <nathanli3636@gmail.com>
2016-07-07 19:55:56 -0700

    update

--- Titanic/Result.R
+++
@@ -1,5 +0,0 @@
-Survived <- predict(fit, test, OOB=TRUE, type = "response")
-predictions <- data.frame(Survived)
-predictions$PassengerId <- df.test$PassengerId
-write.csv(predictions[,c("PassengerId","Survived")],
-    file="desktop/Titanic_pred.csv",row.names=FALSE)
